45eb047a8e2872565e7350658d9603fdbd6fb4f8
Admin <Admin@Admin-PC>
2021-04-06 19:18:40 +0530

    [rameshwar]- sdnjnji

--- src/KITE123/SAMPLEE.java
+++ src/KITE123/SAMPLEE.java
@@ -2,4 +2,9 @@ package KITE123;
 
 public class SAMPLEE {
 
+	
+	public static void main(String[] args) {
+		System.out.println("hiii");
+	}
 }
+// kkk next step sanga ky krtoy te pn sanaga    kyyy  sanga pudhe ky
